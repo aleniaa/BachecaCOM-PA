3f93a9ef918a2d6a819352688061764a769b452e
lab98 <luca.labarbera02@community.unipa.it>
2023-02-16 12:01:11 +0100

    jar sistemate

--- bachecaCOM-PA/src/main/java/model/Allegato.java
+++ bachecaCOM-PA/src/main/java/model/Allegato.java
@@ -5,6 +5,21 @@ public class Allegato {
 	private int idAllegato;
 	private int idAvviso;
 	private String percorso;
+	/**
+	 * @param idAllegato
+	 * @param idAvviso
+	 * @param percorso
+	 */
+	public Allegato(int idAllegato, int idAvviso, String percorso) {
+		this.idAllegato = idAllegato;
+		this.idAvviso = idAvviso;
+		this.percorso = percorso;
+	}
+	/**
+	 * 
+	 */
+	public Allegato() {
+	}
 	
 	
 }
